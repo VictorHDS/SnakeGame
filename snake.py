c34c0d7544dfd7b0916457587abe69d0883f178f
Victor Hugo <victorhugodossantos7@gmail.com>
2021-07-10 12:37:08 -0300

    feat: snake and food components

--- snake.py
+++ snake.py
@@ -10,9 +10,23 @@ curses.curs_set(0) # Hide the cursor
 win.border(0) # Draw the border
 win.nodelay(1) # or -1. We don't wait user hits next key, but continue.
 
+# Snake and Food
+snake = [(4, 10), (4, 9), (4, 8)]
+food = (10, 20)
+
+win.addch(food[0], food[1], '#')
+
 # Game Logic
 score = 0 # 'score' starts at '0'
+
+ESC = 27
+key = curses.KEY_RIGHT
+
+
 while True: # Endless loop
     event = win.getch() # Get next character
+    for c in snake:
+        win.addch(c[0], c[1], '*')
+    win.addch(food[0], food[1], '#')
 curses.endwin() # Destroys window
 print(f'Final score = {score}') # Print the value of 'score' variable on the screen
