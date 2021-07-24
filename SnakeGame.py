700d386c889fe5e2a600eb2a0cf34e807ccb257f
Victor Hugo <victorhugodossantos7@gmail.com>
2021-07-24 13:19:12 -0300

    feat: explanatory comments added

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,8 +1,8 @@
 import pygame
-from pygame.locals import * #From Pygame import all
-pygame.init()
-screen = d=pygame.display.set_mode((600,600))
-pygame.display.set_caption('snake')
+from pygame.locals import * #From Pygame import all locals
+pygame.init()# init pygame game
+screen = d=pygame.display.set_mode((600,600))#Screen Size display
+pygame.display.set_caption('Snake Gamee')
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
