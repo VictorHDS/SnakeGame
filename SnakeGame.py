f3622ea76c680414cbc8bc215359e5df5d1a9ff7
Victor Hugo <victorhugodossantos7@gmail.com>
2021-07-24 13:30:06 -0300

    feat: player('snake') and food('apple') objects

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,10 +1,25 @@
-import pygame
+import pygame, random
 from pygame.locals import * #From Pygame import all locals
+UP=0
+RIGHT=1
+DOWN=2
+LEFT=3
 pygame.init()# init pygame game
 screen = d=pygame.display.set_mode((600,600))#Screen Size display
-pygame.display.set_caption('Snake Gamee')
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
+pygame.display.set_caption('Snake Game')#Window name display
+snake=[(200,200),(210,200),(220,200)]
+snake_skin=pygame.Surface((10,10))
+snake_skin.fill((255,255,255))
+apple_pos=(random.randint(0,590), random.randint(0,590))
+apple=pygame.Surface((10,10))
+apple.fill((255,0,0))
+my_direction=LEFT
+while True: #Infinite loop
+    for event in pygame.event.get():#Get the change events
+        if event.type == QUIT:#When the user close the game
+            pygame.quit()#Quit of the game
+    screen.fill((0,0,0))
+    screen.blit(apple, apple_pos)
+    for pos in snake:
+        screen.blit(snake_skin,pos)
     pygame.display.update()
